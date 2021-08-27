784a36b496ae70ab2cc6d62b87821852f14df59d
Mariana Marcelli <marimmarcelli@gmail.com>
2021-08-27 11:20:15 -0300

    ApiCatalogoJogos

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> ListarJogos(int pagina, int quantidade);
+        Task<JogoViewModel> SelecionarJogoId (Guid id);
+        Task <JogoViewModel> Inserir(JogoInputModel jogo);
+        Task AtualizarJogo(Guid id, JogoInputModel jogo);
+        Task AtualizarPrecoJogo(Guid id, double preco);
+        Task ExcluirJogo(Guid id);
+    }
+}
---
+++ ApiCatalogoJogos/ApiCatalogoJogos/ApiCatalogoJogos/Services/JogoService.cs
@@ -0,0 +1,56 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.Repository;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public class JogoService : IJogoService
+    {
+        private readonly IJogoRepository _jogoRepository;
+
+        public JogoService(IJogoRepository jogoRepository)
+        {
+            _jogoRepository = jogoRepository;
+        }
+
+        public Task AtualizarJogo(Guid id, JogoInputModel jogo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarPrecoJogo(Guid id, double preco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _jogoRepository?.Dispose();
+        }
+
+        public Task ExcluirJogo(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JogoViewModel> Inserir(JogoInputModel jogo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<JogoViewModel>> ListarJogos(int pagina, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JogoViewModel> SelecionarJogoId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+       
+    }
+}
